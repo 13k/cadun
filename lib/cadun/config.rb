be35d74a9c529ca71eee3952fa3fa94852a7cdc9
Kiyoshi '13k' Murata <13k@linhareta.net>
2012-06-11 14:33:55 -0300

    Add possibility to initialize config with a hash

--- lib/cadun/config.rb
+++ lib/cadun/config.rb
@@ -1,8 +1,12 @@
 module Cadun
   class Config    
     def self.load_file(path)
+      load_hash YAML::load_file(path)['cadun']
+    end
+
+    def self.load_hash(hash)
       (class << self; self; end).instance_eval do
-        YAML::load_file(path)['cadun'].each { |key, value| define_method(key) { value } }
+        hash.each { |key, value| define_method(key) { value } }
       end
     end
   end
